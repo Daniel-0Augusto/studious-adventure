0d454b346f3c7f9c52feaf3e2f7720db1091129c
Daniel-0Augusto <127761255+Daniel-0Augusto@users.noreply.github.com>
2025-04-04 08:33:22 -0300

    Add files via upload

--- projeto.js
+++ projeto.js
@@ -1,1 +1,43 @@
-console.log("Hello World!");
+
+let nomeDoHeroi = "Sunlight"
+let xpDoHeroi = 10200
+let rankDoHeroi;
+switch (true) {
+  case xpDoHeroi <= 1000:
+    rankDoHeroi = "Ferro";
+    break;
+  case xpDoHeroi >= 1001 && xpDoHeroi <= 2000:
+    rankDoHeroi = "Bronze";
+    break;
+  case xpDoHeroi > 2000 && xpDoHeroi <= 5000:
+    rankDoHeroi = "Prata";
+    break;
+  case xpDoHeroi >= 5001 && xpDoHeroi <= 7000:
+    rankDoHeroi = "Ouro";
+    break;
+  case xpDoHeroi >= 7001 && xpDoHeroi <= 8000:
+    rankDoHeroi = "Platina";
+    break;
+  case xpDoHeroi >= 8001 && xpDoHeroi <= 9000:
+    rankDoHeroi = "Ascendente";
+    break;
+  case xpDoHeroi >= 9001 && xpDoHeroi <= 10000:
+    rankDoHeroi = "Imortal";
+    break;
+  case xpDoHeroi >= 10001:
+    rankDoHeroi = "Radiante";
+    break;
+}
+console.log(rankDoHeroi)
+
+
+console.log("O Heroi de nome " + nomeDoHeroi, "esta no nivel de " + rankDoHeroi)
+
+//Se XP for menor do que 1.000 = Ferro
+//Se XP for entre 1.001 e 2.000 = Bronze
+//Se XP for entre 2.001 e 5.000 = Prata
+//Se XP for entre 5.001 e 7.000 = Ouro
+//Se XP for entre 7.001 e 8.000 = Platina
+//Se XP for entre 8.001 e 9.000 = Ascendente
+//Se XP for entre 9.001 e 10.000 = Imortal
+//Se XP for maior ou igual a 10.001 = Radiante
